057f095ef7cfb64f69df7050a28ece5558f3bb49
David Neidhart <david.neidhart@gmx.net>
2023-04-17 12:03:13 +0200

    feat: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import { SelectionBuilder } from "./Builder/SelectionBuilder";
-import { Foreground, Background, Cursor } from "./ANSICodes";
+import { PickerBuilder } from "./Builder/PickerBuilder";
+import { MessageBuilder } from "./Builder/MessageBuilder";
 
-export { SelectionBuilder, Foreground, Background, Cursor };
+import { Terminal } from "./Terminal";
+
+import { Foreground, Background, Cursor, Text, getColor as getColorFunction } from "./ANSICodes";
+
+
+export { SelectionBuilder, MessageBuilder, PickerBuilder };
+
+export { Terminal };
+
+export { Foreground, Background, Cursor, Text }
+export { getColor } from "./ANSICodes";
 
 export default class PromptKit {
 	public static get SelectionBuilder() {
 		return SelectionBuilder;
+	}
+
+	public static get PickerBuilder() {
+		return PickerBuilder;
+	}
+
+	public static get MessageBuilder() {
+		return MessageBuilder;
+	}
+
+	public static get Terminal() {
+		return Terminal;
 	}
 
 	public static get Foreground() {
@@ -18,5 +41,13 @@ export default class PromptKit {
 
 	public static get Cursor() {
 		return Cursor;
+	}
+
+	public static get Text() {
+		return Text;
+	}
+
+	public static getColor(color: Foreground | Background | { red: number, green: number, blue: number, background?: boolean }) {
+		return getColorFunction(color);
 	}
 }
