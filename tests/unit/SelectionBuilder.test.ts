4406dd31ab67315d9a268496933215007b61a78f
David Neidhart <david.neidhart@gmx.net>
2023-04-19 22:40:41 +0200

    feat: new selectionbuilder unit-tests

--- tests/unit/SelectionBuilder.test.ts
+++ tests/unit/SelectionBuilder.test.ts
@@ -6,10 +6,21 @@ let builder = new SelectionBuilder();
 
 describe("SelectionBuilder", () => {
 
-	describe("setPrompt", () => {
+	describe("prompt", () => {
+		
+		beforeEach(() => {
+			builder.close();
+			builder = new SelectionBuilder();
+		});
+
 		it("should set the prompt", () => {
 			builder.setPrompt("Select an option");
 			expect(builder.prompt).toBe("Select an option");
+		});
+
+		it('should set a default prompt', () => {
+			const promptString = builder.getPromptString(0, new Set());
+			expect(promptString.split('\n')[0]).toBe('Select some options:');
 		});
 	});
 
@@ -49,6 +60,7 @@ describe("SelectionBuilder", () => {
 
 	describe("Instructions", () => {
 		beforeEach(() => {
+			builder.close();
 			builder = new SelectionBuilder();
 
 			builder.setPrompt("Select an option");
@@ -58,6 +70,11 @@ describe("SelectionBuilder", () => {
 				{ option: 'Option 3', value: 3 },
 				{ option: 'Option 4', value: 4 }
 			]);
+		});
+
+		it('should throw an error if no options are added', () => {
+			builder = new SelectionBuilder();
+			expect(() => builder.build()).toThrowError('No options added');
 		});
 
 		it("should create prompt string", () => {
@@ -97,7 +114,95 @@ describe("SelectionBuilder", () => {
 		});
 	});
 
-	afterAll(() => {
+	describe('Scrolling', () => {
+
+		beforeEach(() => {
+			builder.close();
+			builder = new SelectionBuilder();
+
+			builder.setPrompt("Select an option");
+			builder.addOptions([
+				{ option: 'Option 1', value: 1 },
+				{ option: 'Option 2', value: 2 },
+				{ option: 'Option 3', value: 3 },
+				{ option: 'Option 4', value: 4 },
+				{ option: 'Option 5', value: 5 },
+				{ option: 'Option 6', value: 6 },
+				{ option: 'Option 7', value: 7 },
+				{ option: 'Option 8', value: 8 },
+				{ option: 'Option 9', value: 9 },
+				{ option: 'Option 10', value: 10 },
+				{ option: 'Option 11', value: 11 },
+				{ option: 'Option 12', value: 12 }
+			]);
+		});
+
+		it('should create instruction string, showing only the first 5 options', () => {
+			expect(builder.getPromptString(0, new Set())).toBe(`Select an option
+\x1b[47m\x1b[30m◯ Option 1\x1b[0m
+◯ Option 2
+◯ Option 3
+◯ Option 4
+◯ Option 5
+↑↓: Select option, ␣: Select, ↵: Finish`);
+		});
+
+		it('should create instruction string, showing option 5-9', () => {
+			expect(builder.getPromptString(4, new Set())).toBe(`Select an option
+\x1b[47m\x1b[30m◯ Option 5\x1b[0m
+◯ Option 6
+◯ Option 7
+◯ Option 8
+◯ Option 9
+↑↓: Select option, ␣: Select, ↵: Finish`);
+		});
+
+		it('should create instruction string, showing option 10-12 and 1-2', () => {
+			expect(builder.getPromptString(9, new Set())).toBe(`Select an option
+\x1b[47m\x1b[30m◯ Option 10\x1b[0m
+◯ Option 11
+◯ Option 12
+◯ Option 1
+◯ Option 2
+↑↓: Select option, ␣: Select, ↵: Finish`);
+		});
+	});
+
+	describe('Default option', () => {
+		beforeEach(() => {
+			builder.close();
+			builder = new SelectionBuilder();
+			builder.addOptions([
+				{ option: 'Option 1', value: 1 },
+				{ option: 'Option 2', value: 2 },
+				{ option: 'Option 3', value: 3 },
+				{ option: 'Option 4', value: 4 }
+			])
+			builder.addDefaultOption(0);
+		});
+
+		it('should create instruction string with default option selected', () => {
+			expect(builder.getPromptString(0, builder.defaultOption)).toBe(`Select some options:
+\x1b[47m\x1b[30m◉ Option 1\x1b[0m
+◯ Option 2
+◯ Option 3
+◯ Option 4
+↑↓: Select option, ␣: Select, ↵: Finish (Press ↵ to use the default option)`);
+		});
+
+		it('should create instruction with default options', () => {
+			builder.addDefaultOption(1);
+
+			expect(builder.getPromptString(0, builder.defaultOption)).toBe(`Select some options:
+\x1b[47m\x1b[30m◉ Option 1\x1b[0m
+◉ Option 2
+◯ Option 3
+◯ Option 4
+↑↓: Select option, ␣: Select, ↵: Finish (Press ↵ to use the default options)`);
+		});
+	});
+
+	afterEach(() => {
 		builder.close();
 	});
 });
